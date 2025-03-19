e00ea896c716cc6dd6964d603c275d3d24ce84c0
Ricardo Cantiran <rickycantiran@gmail.com>
2025-03-19 20:39:23 -0300

    Se añadio un sistema de puntaje

--- questions.py
+++ questions.py
@@ -25,7 +25,7 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
-
+puntos = 0.0
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
@@ -47,11 +47,15 @@ for _ in range(3):
         #Se verifica si la respuesta es correcta
         if respuesta == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntos += 1.0
             break
+        else:
+            puntos -= 0.5
     else:
         # Si el usuario no responde correctamente después de 2 intentos, # se muestra la respuesta correcta
          print("Incorrecto. La respuesta correcta es:")
          print(answers[question_index]
         [correct_answers_index[question_index]])
 # Se imprime un blanco al final de la pregunta
-print()
+    print()
+print("su puntaje en el juego fue de :", puntos )
