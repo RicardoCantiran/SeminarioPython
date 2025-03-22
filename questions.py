170b354b54dbd348a8eb97f0867c6729d94f0063
Ricardo Cantiran <rickycantiran@gmail.com>
2025-03-21 22:38:26 -0300

    Se realizo el punto 7.c

--- questions.py
+++ questions.py
@@ -26,26 +26,26 @@ correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
 puntos = 0.0
-for _ in range(3):
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+for questions, answers, question_index in questions_to_ask:
     # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(questions)
+    for i, answer in enumerate(answers):
         print(f"{i + 1}. {answer}")
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        respuesta = input("Respuesta")
+        respuesta = input("Respuesta ")
 
         #compruebo que sea un numero y que este a rango de las preguntas
-        if(respuesta.isdigit() and int(respuesta) > 0 and int(respuesta) < 4 ):
+        if(respuesta.isdigit() and int(respuesta) > 0 and int(respuesta) < 5 ):
             respuesta = int(respuesta) -1
         else:
             #sino fuerzo un cierre del programa con exit
             print ("respuesta invalida")
             exit(1)
         #Se verifica si la respuesta es correcta
-        if respuesta == correct_answers_index[question_index]:
+        if respuesta == question_index:
             print("¡Correcto!")
             puntos += 1.0
             break
@@ -54,8 +54,7 @@ for _ in range(3):
     else:
         # Si el usuario no responde correctamente después de 2 intentos, # se muestra la respuesta correcta
          print("Incorrecto. La respuesta correcta es:")
-         print(answers[question_index]
-        [correct_answers_index[question_index]])
+         print(answers[question_index])
 # Se imprime un blanco al final de la pregunta
     print()
 print("su puntaje en el juego fue de :", puntos )
